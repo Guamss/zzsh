c197bbbdb4311f902aeac7e94fe7b790f3621eaf
guamss <guamsmdr@gmail.com>
2023-06-28 10:32:51 +0200

    (feat): execution de commande et redirection de l'entrée standard

--- exec/exec.c
+++ exec/exec.c
@@ -1,3 +1,19 @@
-int execute()
+#include <stdio.h>
+#include "../cmd/cmd.h"
+
+int execute(cmd* input, char** env)
 {
+  for(int i=0; i++;input[i]!=NULL)
+  {
+    // création d'un processus enfant du programme
+    cont int pid = fork();
+    // pid de l'enfant est 0
+    if (pid == 0) 
+    {
+      // redirige la sortie standard vers un file descriptor
+      dup2(1, input.fd_out); 
+      // on tue l'enfant (l'enfant devient la commande entrée par l'utilisateur)
+      execve(input[i].executable, input[i].args, env);
+    }
+  }
 }
