451a34561d6ef25e877911fe71ea3ba209faa49f
starnakin <camille@chauvet.pro>
2023-06-30 14:41:11 +0200

    fix: command not found

--- src/exec/exec.c
+++ src/exec/exec.c
@@ -101,7 +101,9 @@ int cmds_list_exec(lst** cmds, lst** env)
 			content->fd_out = fds[1];
 			((cmd*)current->next->content)->fd_in = fds[0];
 		}
-		if (builtin_execute(content, env) == 1)
+		if (content->executable == NULL)
+			dprintf(2, "zzsh: command not found: %s\n", content->args[0]);
+		else if (builtin_execute(content, env) == 1)
 			execute(content, env);
 		current = current->next;
 	}
